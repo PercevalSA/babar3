61fc60cc8377979ee6c66813cf417bfe41829586
Dryvenn <dryvenn@gmail.com>
2016-02-20 18:22:46 +0100

    Add basic autocompletes

--- front/app/scripts/controllers/main.js
+++ front/app/scripts/controllers/main.js
@@ -8,9 +8,35 @@
  * Controller of the BabarApp
  */
 angular.module('BabarApp')
-.controller('MainCtrl', function ($scope, API) {
-	this.customers = [];
+.controller('MainCtrl', function ($scope, $mdDialog, $mdDialogPreset, API) {
 	API.getCustomer().then(function(res) {
 		$scope.main.customers = res.data;
 	});
+	this.setCustomer = function(pk) {
+		if(pk) {
+			API.getCustomer(pk).then(function(res) {
+				$scope.main.customer = res.data;
+			});
+		}
+		else {
+			$scope.main.customer = null;
+		}
+	};
+
+	API.getProduct().then(function(res) {
+		$scope.main.products = res.data;
+	});
+	this.setProduct = function(pk) {
+		if(pk) {
+			API.getProduct(pk).then(function(res) {
+				$scope.main.product = res.data[0];
+			});
+		}
+		else {
+			$scope.main.product = null;
+		}
+	};
+
+	this.makePurchase = function() {
+	};
 });
