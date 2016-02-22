c639830425622ad5acb43791de729bb3bd571a20
Dryvenn <dryvenn@gmail.com>
2016-02-22 19:58:29 +0100

    Delete money field in API

--- back/babar_server/serializers.py
+++ back/babar_server/serializers.py
@@ -3,28 +3,47 @@ from .models import *
 
 
 class StatusSerializer(serializers.ModelSerializer):
+    """
+    Status's view is read-only anyway
+    """
     class Meta:
         model = Status
         fields = ('pk', 'name', 'overdraft')
 
+
 class ProductSerializer(serializers.ModelSerializer):
+    """
+    Product's view is read-only anyway
+    """
     class Meta:
         model = Product
         fields = ('pk', 'name', 'price')
 
+
 class CustomerSerializer(serializers.ModelSerializer):
+    """
+    Customer's view is read-only anyway
+    """
     class Meta:
         model = Customer
         fields = ('pk', 'firstname', 'lastname', 'nickname', 'fullname', 'email', 'status', 'balance')
 
+
 class PaymentSerializer(serializers.ModelSerializer):
+    """
+    Payment is not read-only: specify the fields which are
+    """
     class Meta:
         model = Payment
         fields = ('pk', 'customer', 'money')
         read_only_fields = ('timestamp')
 
+
 class PurchaseSerializer(serializers.ModelSerializer):
+    """
+    Purchase is not read-only: specify the fields which are
+    """
     class Meta:
         model = Purchase
-        fields = ('customer', 'product', 'money')
+        fields = ('customer', 'product')
         read_only_fields = ('timestamp')
