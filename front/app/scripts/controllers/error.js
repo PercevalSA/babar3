c58aa103e28e2efeb5d5134434ba5db7c311f82c
Dryvenn <dryvenn@gmail.com>
2016-02-26 10:15:05 +0100

    Simplify error view

--- front/app/scripts/controllers/error.js
+++ front/app/scripts/controllers/error.js
@@ -9,70 +9,32 @@
  */
 angular.module('BabarApp')
 .controller('ErrorCtrl', function ($routeParams) {
-	var getStatus = function() {
-		if($routeParams.status) {
-			return $routeParams.status;
-		}
-		else {
-			return '0';
-		}
-	};
 
-	var getMessage = function() {
-		switch(getStatus()) {
-			case '200':
-				return 'Everything is allright.';
+	var getMessage = function(status) {
+		var message = '';
+		switch(status) {
 			case '400':
-				return 'This is a bad request.';
-			case '401':
-				return 'This operation requires an authentication.';
-			case '403':
-				return 'Those are incorrect credentials (login/password).';
+				message = 'Bad request.';
+			break;
 			case '404':
-				return 'This content could not be found on server.';
-			case '405':
-				return 'Ouch! The client encountered an unexpected error.';
-			case '409':
-				return 'This content already exists on server.';
+				message = 'Not found.';
+			break;
 			case '418':
-				return 'The server\'s saying she is a keg.';
-			case '420':
-				return 'The app did not meet the user\'s expectations.';
-			case '498':
-				return 'This session has expired, please log in.';
-			default:
-				return 'Ouch! The application encountered an unexpected error.';
-		}
-	};
-
-	var isContactNeeded = function() {
-		switch(getStatus().toString()) {
+				message = 'I\'m a keg.';
+			break;
 			case '200':
-				return false;
-			case '400':
-				return true;
+			case '201':
 			case '401':
-				return false;
 			case '403':
-				return false;
-			case '404':
-				return false;
-			case '405':
-				return true;
-			case '409':
-				return false;
-			case '418':
-				return false;
-			case '420':
-				return true;
-			case '498':
-				return false;
+				message = 'Ouch! This error (' + status + ') should not lead here!';
+			break;
 			default:
-				return true;
+				message = 'Ouch! This error (' + status + ') is unknown!';
+			break;
 		}
+		return message;
 	};
 
-	this.status = getStatus();
-	this.message = getMessage();
-	this.needToContact = isContactNeeded();
+	this.status = $routeParams.status.toString();
+	this.message = getMessage(this.status);
 });
