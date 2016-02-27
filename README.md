c231f770219bf44fe5ff5983af2f0d26ac93d2b5
Dryvenn <dryvenn@gmail.com>
2016-02-27 18:40:09 +0100

    Add a migration script

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
-# Babar3
-
-## Installation
-### Back
+# Installation
+## Back
 - Make sure you have `mysql-server`, `python3`, and `virtualenv`
 - Create your MySQL database
 - Fill in the database info in `settings.py`
@@ -18,7 +16,7 @@ At this point, you should be able to access to a local development server.
 If you encounter MySQL errors, check user permissions and password settings in the MySQL console.
 To exit virtualenv, use `deactivate`.
 
-### Front
+## Front
 - Make sure you have `nodejs`, and `npm`.
 - Run the following commands:
 ```bash
@@ -30,32 +28,39 @@ grunt serve
 At this point you should be able to access a local server providing the website.
 
 
-## Testing
-### Back
+# Testing
+## Back
 Run `python3 manage.py test`.
 
-### Front
+## Front
 Run `grunt test`.
 
 
-## Deployment
+# Migration
+To migrate the old babar database to the new one:
+- First get a JSON dump of the old DB
+- Convert it with `migrate.py old.json`
+- Import it in Django `python3 manage.py loaddata new.json`
+
+
+# Deployment
 First, go through both installation phases.
-### Back
+## Back
 Read the [Django checklist](https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/)
 TODO: add here django checklist points (change secure key)
 TODO: setup https cors
 
-### Front
+## Front
 Run `grunt` to build.
 
-### Setup the server
-#### Setup the Python interface
+## Setup the server
+### Setup the Python interface
 ```bash
 source back/env/bin/activate
 gunicorn babar3:wsgi
 ```
 
-#### Setup the WWW interface
+### Setup the WWW interface
 Make sure you have `nginx`, and use [this config](https://github.com/benoitc/gunicorn/blob/master/examples/nginx.conf).
 In the config file, enable HTTPS.
 ```bash
@@ -63,19 +68,19 @@ sudo nginx -s reload
 ```
 
 
-## Documentation
-### Back
+# Documentation
+## Back
 - [Django](https://www.djangoproject.com/)
 - [Django REST framework](http://www.django-rest-framework.org/)
 - [Django REST auth](https://django-rest-auth.readthedocs.org)
 
-### Front
+## Front
 - [Yeoman: generator-angular](https://github.com/yeoman/generator-angular)
 - [Angular](https://docs.angularjs.org/)
 - [Angular Material Design](https://material.angularjs.org/latest/)
 
 
-## TODO
+# TODO
 - Make the website responsive
 - Add a filter to the API for purchases and payments
 - Handle automatic server logouts
---
+++ migrate.py
@@ -0,0 +1,211 @@
+#!/usr/local/bin/python3
+
+import sys
+import os
+import json
+import random
+import string
+
+""" Quick and dirty migration script
+It is not optimized at all and run quite slowly.
+It deals arbitrary with errors the following way:
+    - names of all kind are truncated to 25 chars
+    - if no email, it is randomidied and use domain example.org
+    - nicknames not unique are prepended with digits
+    - epoch timestamps become all ones
+    - deleted customers loose their payments
+    - status is either VIP or barman, or defaults to regular
+    - quantities make multiple purchases
+"""
+
+
+class Model:
+    db = 'babar_server.'
+    pk = 1
+
+    def __init__(self):
+        self.pk = self.__class__.pk
+        self.__class__.pk += 1
+
+    def encode(self):
+        return {
+            'model': Model.db + self.__class__.__name__.lower(),
+            'pk': self.pk,
+            'fields': {}
+        }
+
+
+class Status(Model):
+    pk = 1
+
+    def __init__(self, name, overdraft):
+        super().__init__()
+        self.name = name
+        self.overdraft = overdraft
+
+    def encode(self):
+        data = super().encode()
+        data['fields'] = {
+            'name': self.name,
+            'overdraft': self.overdraft
+        }
+        return data
+
+
+class Product(Model):
+    pk = 1
+
+    def __init__(self, name, price):
+        super().__init__()
+        self.name = name[0:25]
+        self.price = price
+
+    def encode(self):
+        data = super().encode()
+        data['fields'] = {
+            'name': self.name,
+            'price': self.price
+        }
+        return data
+
+
+class Payment(Model):
+    pk = 1
+
+    def __init__(self, customer, amount, timestamp):
+        super().__init__()
+        self.customer = customer
+        self.amount = amount
+        if timestamp == '0000-00-00T00:00:00.000Z':
+            timestamp = timestamp.replace('0', '1')
+        self.timestamp = timestamp
+
+    def encode(self):
+        data = super().encode()
+        data['fields'] = {
+            'customer': self.customer,
+            'amount': self.amount,
+            'timestamp': self.timestamp
+        }
+        return data
+
+
+class Purchase(Payment):
+    pk = 1
+
+    def __init__(self, customer, product, amount, timestamp):
+        super().__init__(customer, amount, timestamp)
+        self.product = product
+
+    def encode(self):
+        data = super().encode()
+        data['fields']['product'] = self.product
+        return data
+
+
+class Customer(Model):
+    pk = 1
+
+    def __init__(self, firstname, lastname, nickname, status):
+        super().__init__()
+        self.firstname = firstname[0:25]
+        self.lastname = lastname[0:25]
+        self.nickname = nickname[0:25]
+        self.email = ''.join(random.choice(string.digits) for _ in range(10)) + '@example.org'
+        self.status = status
+
+    def encode(self):
+        data = super().encode()
+        data['fields'] = {
+            'firstname': self.firstname,
+            'lastname': self.lastname,
+            'nickname': self.nickname,
+            'email': self.email,
+            'status': self.status
+        }
+        return data
+
+
+if len(sys.argv) != 2 or not os.path.isfile(sys.argv[1]):
+    print("Usage: ./migrate.py old.json")
+if os.path.isfile('./new.json'):
+    print("./new.json would get overwritten. Please remove it yourself.")
+    sys.exit(1)
+
+
+with open(sys.argv[1], 'r') as f:
+    lines = f.readlines()
+
+out = []
+
+for index, line in enumerate(lines):
+    if "babar_legacy." in line:
+        label = line[line.find("babar"):].rstrip()
+        if 'cards' in line or 'card_types' in line or 'managers' in line:
+            print("Skipping " + label + ".")
+        elif 'status' in line:
+            print("Making up new statuses: 'Regular', 'Barman' and 'VIP'.")
+            out += [Status("Regular", 0), Status("Barman", 20), Status("VIP", 80)]
+
+        else:
+            print("Parsing " + label + ".")
+            # next line is empty, the one after is data
+            data = json.loads(lines[index + 2])
+            if 'customers' in line:
+                for obj in data:
+                    status = 1
+                    if 'barman' in obj['status'].lower():
+                        status = 2
+                    if 'vip' in obj['status'].lower():
+                        status = 3
+                    if not obj['nickname']:
+                        nickname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+                    else:
+                        nickname = obj['nickname']
+                        for out_obj in out:
+                            if type(out_obj) is Customer:
+                                if out_obj.nickname.lower() == nickname.lower():
+                                    nickname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+                    o = Customer(obj['surname'], obj['name'], nickname, status)
+                    o.id = obj['customer_id'] # remember old id
+                    out += [o]
+            elif 'drinks' in line:
+                for obj in data:
+                    o = Product(obj['name'], obj['price'])
+                    o.id = obj['drink_id'] # remember old id
+                    out += [o]
+
+for index, line in enumerate(lines):
+    if "babar_legacy." in line:
+        if 'entries' in line or 'sells' in line:
+            label = line[line.find("babar"):].rstrip()
+            print("Parsing " + label + ".")
+            # next line is empty, the one after is data
+            data = json.loads(lines[index + 2])
+            for obj in data:
+                c_pk = None
+                p_pk = None
+                time = obj['date'].replace(' ', 'T') + '.000Z'
+                for inst in out:
+                    if type(inst) is Customer and inst.id == obj['customer_id']:
+                        c_pk = inst.pk
+                if c_pk is not None: # ignore payments and purchases of dead customers
+                    if 'entries' in line:
+                        o = Payment(c_pk, obj['amount'], time)
+                        out += [o]
+                    else:
+                        for inst in out:
+                            if type(inst) is Product and inst.id == obj['drink_id']:
+                                p_pk = inst.pk
+                        if p_pk is None:
+                            raise Exception('This obj' + str(obj) + ' has no product')
+                        for i in range(int(obj['quantity'])):
+                            o = Purchase(c_pk, p_pk, obj['price'], time)
+                            out += [o]
+
+
+# Convert our instances to valid DB dictionaries
+out = [inst.encode() for inst in out]
+
+with open("./new.json", 'w') as f:
+    json.dump(out, f, indent=4, sort_keys=True)
