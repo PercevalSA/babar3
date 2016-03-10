8c47dd67d4035ad9ad50adcd3bf3c022875b6284
Dryvenn <dryvenn@gmail.com>
2016-03-11 00:37:53 +0100

    Put the auth controller alongside its directive

--- front/app/scripts/services/auth.js
+++ front/app/scripts/services/auth.js
@@ -8,17 +8,51 @@
  * Service in the BabarApp.
  */
 angular.module('BabarApp')
-  .service('auth', function ($mdDialog) {
-	  this.prompt = function(why) {
-		  return $mdDialog.show({
-			  templateUrl: '../views/auth.html',
-			  openFrom: '#left',
-			  closeTo: '#right',
-			  clickOutsideToClose: true,
-			  escapeToClose: true,
-			  controller: 'AuthCtrl',
-			  controllerAs: 'auth',
-			  locals: {why: why},
-		  });
-	  };
-  });
+.controller('AuthCtrl', function ($location, $scope, $mdDialog, API, why) {
+	this.why = why;
+
+	this.cancel = function() {
+		$mdDialog.cancel();
+	};
+	this.submit = function() {
+		/* Try an authentication
+		 * If it fails, demand another password or a cancellation
+		 * It it succeeds, resolve the dialog;
+		 * this will retrigger the previous request.
+		 */
+		API.login(this.username, this.password)
+		.then(function(res)  {
+			// Auth successful, store the token and exit
+			API.setToken(res.data.key);
+			$mdDialog.hide();
+		}, function(res) {
+			// Auth unsuccessful, tell the user
+			if(res.status === 400) {
+				$scope.authForm.password.$error.wrong = true;
+			}
+			else {
+				$location.url('error');
+			}
+		});
+	};
+})
+.service('auth', function ($mdDialog, $mdToast) {
+	this.prompt = function(why) {
+		return $mdDialog.show({
+			templateUrl: '../views/auth.html',
+			openFrom: '#left',
+			closeTo: '#right',
+			clickOutsideToClose: true,
+			escapeToClose: true,
+			controller: 'AuthCtrl',
+			controllerAs: 'auth',
+			locals: {why: why},
+		}).then(function() {
+			// OK
+			// Don't say nothing, OP will talk
+		}, function() {
+			// Cancelled
+			$mdToast.showSimple('Cancelled');
+		});
+	};
+});
