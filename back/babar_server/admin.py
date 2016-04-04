1dd60cb82abe86dbabd3e1b4e32d39e36adb50c5
Dryvenn <dryvenn@gmail.com>
2016-04-04 22:44:09 +0200

    Prevent the creation of new superusers from admin

--- back/babar_server/admin.py
+++ back/babar_server/admin.py
@@ -49,35 +49,47 @@ class CustomerAdmin(admin.ModelAdmin):
 
 class RestrictiveUserAdmin(UserAdmin):
     """
-    Do not allow privilege escalation!
-    If someone that is not a superuser try to access one,
-    do not display sensitive fields.
+    We want to prevent privilege escalation in the admin site.
+    - no restrictions for superusers;
+    - non-superusers cannot make new ones.
     Based on http://stackoverflow.com/a/2298268
     """
     restricted_fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        #(_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'groups')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
 
     def change_view(self, request, *args, **kwargs):
-        targeted_user = User.objects.get(pk=args[0])
-        if request.user.is_superuser or not targeted_user.is_superuser:
-            # Either the current user is superuser
-            # Or the targeted user is not superuser
-            # So adopt the standard behavior
+        # Two users to take into account, current and target.
+        target = User.objects.get(pk=args[0])
+        current = request.user
+
+        # No restrictions for superusers
+        if current.is_superuser:
             return super(RestrictiveUserAdmin, self).change_view(request, *args, **kwargs)
-        else:
+        # No modifications to superusers
+        elif target.is_superuser:
             try:
                 # All fields are made read-only
-                readonly = []
+                self.readonly_fields = []
                 for fs in self.fieldsets:
-                    readonly += list(fs[1]['fields'])
-                self.readonly_fields = readonly
+                    self.readonly_fields += list(fs[1]['fields'])
                 response = super(RestrictiveUserAdmin, self).change_view(request, *args, **kwargs)
             finally:
                 # Reset read-only fields
                 self.readonly_fields = UserAdmin.readonly_fields
+            return response
+        # Restrictions for normal users
+        else:
+            try:
+                self.fieldsets = self.restricted_fieldsets
+                response = super(RestrictiveUserAdmin, self).change_view(request, *args, **kwargs)
+            finally:
+                # Reset fields
+                self.fieldsets = UserAdmin.fieldsets
             return response
 
 
